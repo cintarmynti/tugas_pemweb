6119e2b0c36cd53566712d2608822653bbadbb95
Cinta Ramayanti <58018808+cintarmynti@users.noreply.github.com>
2024-05-26 08:07:43 +0700

    alert register

--- functions/authcode.php
+++ functions/authcode.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();
 include('../config/dbcon.php');
 if (isset($_POST['register_btn'])) {
     $name = mysqli_real_escape_string($con, $_POST['name']);
@@ -8,21 +8,34 @@ if (isset($_POST['register_btn'])) {
     $password = mysqli_real_escape_string($con, $_POST['password']);
     $cpassword = mysqli_real_escape_string($con, $_POST['cpassword']);
 
-    if ($password == $cpassword) {
-        // insert user 
-        $insert_query = "INSERT INTO users (name, email,phone,password) VALUES ('$name','$email', '$phone','$password')";
-        $insert_query_run = mysqli_query($con, $insert_query);
 
-        if ($insert_query_run) {
-            $_SESSION['message'] = "Registered Successfully";
-            header('Location: ../login.php');
+    // cek apabila email sudah ada 
+    $check_email_query = "SELECT email FROM users WHERE email='$email'";
+    $check_email_query_run = mysqli_query($con, $check_email_query);
+
+    if(mysqli_num_rows($check_email_query_run) > 0)
+    {
+        $_SESSION['message'] = "Email already registered";
+        header('Location: ../register.php');
+    }else{
+        if ($password == $cpassword) {
+            // insert user 
+            $insert_query = "INSERT INTO users (name, email,phone,password) VALUES ('$name','$email', '$phone','$password')";
+            $insert_query_run = mysqli_query($con, $insert_query);
+    
+            if ($insert_query_run) {
+                $_SESSION['message'] = "Registered Successfully";
+                header('Location: ../login.php');
+            } else {
+                $_SESSION['message'] = "Something went wrong";
+                header('Location: ../register.php');
+            }
+    
         } else {
-            $_SESSION['message'] = "Something went wrong";
+            $_SESSION['message'] = "Password Do Not Match";
             header('Location: ../register.php');
         }
-
-    } else {
-        $_SESSION['message'] = "Password Do Not Match";
-        header('Location: ../register.php');
     }
+
+   
 }
