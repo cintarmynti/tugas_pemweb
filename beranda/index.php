effd870e93d44ce78a20c83491cf3c50e95ee75e
Dela Puspita Lasminingrum <150015004+Delapuspita77@users.noreply.github.com>
2024-06-05 10:40:08 +0700

    .

---
+++ beranda/index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PEMWEB</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container-fluid">
+          <a class="navbar-brand me-auto" href="#">Magetan</a>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Magetan Travel Guide</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body mx-auto">
+              <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link mx-lg-2" aria-current="page" href="/views/destinasiwisata.html">Destinasi Wisata</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mx-lg-2" href="/views/tempatmakan.html">Tempat Makan</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mx-lg-2" href="/views/penginapan.html">Penginapan</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mx-lg-2 active" href="/views/index.html">Beranda</a>
+                </li>
+              </ul>
+            </div>
+            
+          </div>
+          <a href="#" class="login-button">Login</a>
+            <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+  <!-- <nav class="navbar" id="navbar">
+      <div class="logo">Magetan</div>
+      <div class="menu">
+          <ul>
+              <li> <a href="#destinasi">Destinasi Wisata</a></li>
+              <li> <a href="/views/tempatmakan.html">Tempat Makan</a></li>
+              <li> <a href="#penginapan">Penginapan</a></li>
+          </ul>    
+      </div>
+      <div class="home">
+          <a href="#home">Home</a>
+      </div>
+  </nav> -->
+
+  <section id="homepage1">
+    <div class="hero">
+        <div class="hero-text">
+            <h1>Nikmati pesona <br> alam di Kabupaten <br> Magetan</h1>
+        </div>
+    </div>
+  </section>
+
+  <section id="homepage2">
+  <div class="grup1">
+      <h1>KEINDAHAN <br> KOTA MAGETAN</h1>
+      <p>Magetan, tempat di mana alam <br> berbicara lewat pemandangan <br> pegunungan yang memukau dan <br> danau-danau yang menenangkan</p>
+  </div>
+
+  <div class="grup2">
+      <div class="slider">
+        <a href="#slide-1">1</a>
+        <a href="#slide-2">2</a>
+        <a href="#slide-3">3</a>
+      <div class="slides">
+          <div id="slide-1">
+          <img src="/assets/Rectangle3.png" alt="">
+          </div>
+          <div id="slide-2">
+          <img src="/assets/Rectangle4.png" alt="">
+          </div>
+          <div id="slide-3">
+          <img src="/assets/Rectangle5.png" alt="">
+          </div>
+      </div>
+      </div>
+  </div> 
+  </section>
+
+  <section id="homepage3">
+    <div class="gambar">
+        <img src="/assets/Frame6.png" alt="">
+    </div>
+    <div class="tentang">
+        <h1>TENTANG<br> KOTA MAGETAN</h1>
+        <p>Magetan, tempat di mana alam <br> berbicara lewat pemandangan <br> pegunungan yang memukau dan <br> danau-danau yang menenangkan</p>
+    </div>
+  </section>
+
+  <section id="homepage4">
+    <div class="jelajahi">
+        <h1>JELAJAHI</h1>
+        <div class="wisatapreview">
+            <div class="text1">
+                <h2> <a href="#judul1">JUDUL REVIEW</a></h2>
+                <p>Pemandangan yang menyejukkan hati,<br> dimana kita dapat melihat bunga-bunga <br>yang indah.</p>
+            </div>
+            <img src="/assets/gambar1.png" alt="">
+        </div>
+        <div class="wisatapreview">
+            <div class="text2">
+                <h2> <a href="#judul2">JUDUL REVIEW</a></h2>
+                <p>Pemandangan yang menyejukkan hati,<br> dimana kita dapat melihat bunga-bunga <br>yang indah.</p>
+            </div>
+            <img src="/assets/gambar2.png" alt="">
+        </div>
+        <div class="wisatapreview">
+            <div class="text3">
+                <h2> <a href="#judul3">JUDUL REVIEW</a></h2>
+                <p>Pemandangan yang menyejukkan hati,<br> dimana kita dapat melihat bunga-bunga <br>yang indah.</p>
+            </div>
+            <img src="/assets/gambar3.png" alt="">
+        </div>
+        <div class="wisatapreview">
+            <div class="text4">
+                <h2> <a href="#judul4">JUDUL REVIEW</a></h2>
+                <p>Pemandangan yang menyejukkan hati,<br> dimana kita dapat melihat bunga-bunga <br>yang indah.</p>
+            </div>
+            <img src="/assets/gambar4.png" alt="">
+        </div>
+    </div>
+
+    <div class="kuliner">
+        <h1>KULINER</h1>
+        <div class="kulinerpreview">
+            <div class="satu">
+                <img src="/assets/Ellipse1.png" alt="">
+                <p><a href="#kuliner1">Sate Kelinci</a></p>
+            </div>
+        </div>
+        <div class="kulinerpreview">
+            <div class="satu">
+                <img src="/assets/Ellipse2.png" alt="">
+                <p><a href="#kuliner1">Nasi Pecel</a></p>
+            </div>
+        </div>
+        <div class="kulinerpreview">
+            <div class="satu">
+                <img src="/assets/Ellipse3.png" alt="">
+                <p><a href="#kuliner1">Sop Buntut</a></p>
+            </div>
+        </div>
+        <div class="kulinerpreview">
+            <div class="satu">
+                <img src="/assets/Ellipse4.png" alt="">
+                <p><a href="#kuliner1">Bakso Magetan</a></p>
+            </div>
+        </div>
+    </div>
+  </section>
+
+  <footer>
+    <p>&copy; copyright 2024 Kelompok 6 Pemrograman Web A-081</p>
+    <p>
+        <a href="#home">Home</a> |
+        <a href="#about">About Us</a> |
+        <a href="#contact">Contact</a>
+    </p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+  <script src="/scripts/script.js"></script>
+</html>
