c073cd297ec526213490593f0662210d5019ade5
Cinta Ramayanti <58018808+cintarmynti@users.noreply.github.com>
2024-06-07 08:12:55 +0700

    footer

---
+++ views/wisata/detail.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Geni Langit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="./style-new-page.css">
+</head>
+<body>
+    <?php include('../../includes/navbar.php') ?>
+    
+    <div class="container-fluid banner-geni-langit">
+        <div class="container text-center">
+          <div class="overlay"></div>
+          <div class="content">
+              <h4 class="display-6">Selamat Datang Di Geni Langit</h4>
+              <h3 class="display-3">Nikmati pesonanya</h3>
+              <a href="#lokasi" class="login-button">Menuju Tempat</a>
+                <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+          </div>
+            
+        </div>
+    </div>
+    <div class="container-fluid deskripsi pt-5 pb-5">
+        <div class="container text-center">
+            <h2 class="display-3" id="deskripsi">
+                Deskripsi
+            </h2>
+            <p>Taman Wisata Genilangit merupakan potensi dari desa wisata yang menawarkan keindahan alam dan budaya. Selain itu, terdapat fasititas berupa taman, rumah pohon, hammock atau tempat tidur gantung, serta kafe yang semakin menambah kenyaman pengunjung dalam menikmati pesona pohon pinus.</p>
+            <div class="row pt-4">
+              <div class="col-md-4">
+                <span class="circle">
+                  <i class="fa-solid fa-circle-info fa-5x"></i>
+                </span>
+                
+                <h3 class="mt-3"> Tentang </h3>
+                <p>Wisata Genilangit menawarkan pemandangan alam, spot foto menarik, dan berbagai wahana seru di pegunungan Magetan.</p>
+
+                
+              </div>
+
+              <div class="col-md-4 ">
+                <span class="circle">
+                  <i class="fa-solid fa-circle-dollar-to-slot fa-5x"></i>
+                </span>
+                
+                <h3 class="mt-3"> Harga Tiket Masuk</h3>
+                <p>Tiket Masuk :<br>
+                  Dewasa Rp 10.000<br>
+                  Anak-anak Rp 10.000</p>
+
+                
+              </div>
+
+              <div class="col-md-4">
+                <span class="circle">
+                  <i class="fa-solid fa-mountain-sun fa-5x"></i>
+                </span>
+                
+                <h3 class="mt-3">Aktivitas dan Fasilitas</h3>
+                <p>Genilangit memiliki aktivitas berupa spot foto yang instagramable, taman bermain, taman bunga, wisata berkuda, ATV, flying fox, kegiatan outbound. memiliki berbaga fasilitas mulai dari area parkir, mushola, gazebo, toilet, hingga warung makan.</p>
+
+                
+              </div>
+            </div>
+
+            
+            
+
+        </div>
+    </div>
+    <div class="container-fluid pt-5 pb-5" id="lokasi">
+      <div class="container text-center">
+        <h2 class="display-3">Lokasi</h2>
+        <p>Genilangit terletak di Poncol, Magetan, Jawa Timur. Destinasi wisata ini berada di dataran tinggi, menawarkan udara sejuk dan pemandangan pegunungan yang indah, serta mudah diakses dari pusat kota Magetan.
+        </p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31630.49191775358!2d111.19243639123026!3d-7.703368508097404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e798ee78a1e8a07%3A0xbd6e9bc69c4df2b9!2sGenilangit%2C%20Kec.%20Poncol%2C%20Kabupaten%20Magetan%2C%20Jawa%20Timur!5e0!3m2!1sid!2sid!4v1717389998443!5m2!1sid!2sid" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+    </div>
+    <section class="client">
+      <div class="card mx-auto pl-5 mr-5 pt-5 pb-5 section_container client_container">
+        <h2 class="text-center section__header">What our client say</h2>
+        <div class="mx-auto ml-3 mr-3 client__grid">
+          <div class="card mx-auto pl-5 ml-3 client__card" style="width: 18rem;">
+            <img src="../../assets/img/client-1.jpg" alt="client" />
+            <p>
+              The booking process was seamless, and the confirmation was
+              instant. I highly recommend WDM&Co for hassle-free hotel bookings.
+            </p>
+          </div>
+          <div class="card mx-auto ml-3 client__card" style="width: 18rem;">
+            <img src="../../assets/img/client-2.jpg" alt="client" />
+            <p>
+              The website provided detailed information about hotel, including
+              amenities, photos, which helped me make an informed decision.
+            </p>
+          </div>
+          <div class="card mx-auto client__card" style="width: 18rem;">
+            <img src="../../assets/img/client-3.jpg" alt="client" />
+            <p>
+              I was able to book a room within minutes, and the hotel exceeded
+              my expectations. I appreciate WDM&Co's efficiency and reliability.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="comment-area pt-5 pb-5">
+      <h1>Masukkan Komentar Anda</h1>
+      <textarea class="form-control" placeholder="Tulis sesuatu di sini..."></textarea>
+  </div>
+
+  <br>
+    
+  <?php include('../../includes/footer.php') ?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
